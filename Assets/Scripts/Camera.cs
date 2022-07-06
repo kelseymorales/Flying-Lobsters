4df951d5f9d982933c1eebb48b339319af23c599
Tyler Clardy <tclards@gmail.com>
2022-07-06 15:31:05 -0500

    Created the Player prefab, including the muzzleFlash effect and the Hit Effect - Spark prefab. Created the playerController script to tie in with it.
    Created the camera script, and attached it to the player prefab.
    
    Added the Shoot and Sprint input to the project settings - input manager.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] int iSenseHori; // Sensitivity horizontally
+    [SerializeField] int iSenseVert; // Sensitivity vertically
+
+    // Set field that camera can operate in (in a range min to max)
+    [SerializeField] int iLockVertMin;
+    [SerializeField] int iLockVertMax;
+
+    [SerializeField] bool isYAxisInverted; // toggle for inverted camera controls
+
+    float fXAxisRotation = 0.0f;
+
+    // Called at Start
+    void Start()
+    {
+        // Lock cursor to center of screen & make it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void LateUpdate()
+    {
+        // get input
+        float mouseX = Input.GetAxis("Mouse X") * iSenseHori * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * iSenseVert * Time.deltaTime;
+
+        // inverted controls check/handling
+        if (isYAxisInverted)
+        {
+            fXAxisRotation += mouseY;
+        }
+        else
+        {
+            fXAxisRotation -= mouseY;
+        }
+
+        // Clamp the angle the camera can rotate to
+        fXAxisRotation = Mathf.Clamp(fXAxisRotation, iLockVertMin, iLockVertMax);
+
+        // Rotate camera on the x-axis
+        transform.localRotation = Quaternion.Euler(fXAxisRotation, 0, 0);
+
+        // Rotate the parent (player) transform to follow camera
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,31 +1,172 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
-public class playerController : MonoBehaviour
+public class playerController : MonoBehaviour, IDamageable
 {
-    [SerializeField] CharacterController controller;
-    [SerializeField] int HP;
-    int HPorig;
-    Vector3 playerSpawnPos;
+    [Header("Components")]
+    [SerializeField] CharacterController _controller;
 
-    // Start is called before the first frame update
+    [Header("Player Attributes")]
+    [Header("--------------------------")]
+    [Range(5, 20)][SerializeField] int iPlayerHealth;
+    [Range(3, 6)][SerializeField] float fPlayerSpeed;
+    [Range(1.5f, 4.0f)][SerializeField] float fSprintMulti; // multiplier for increasing player speed during use of the sprint() function
+    [Range(6, 10)][SerializeField] float fJumpHeight;
+    [Range(15, 30)][SerializeField] float fGravityValue;
+    [Range(1, 4)][SerializeField] int iJumps; // Max jumps allowed
+
+    [Header("Player Weapon Stats")]
+    [Header("-------------------------")]
+    [Range(0.1f, 3.0f)][SerializeField] float fShootRate;
+    [Range(1, 10)][SerializeField] int iWeaponDamage;
+
+    [Header("Effects")]
+    [Header("--------------------------")]
+    [SerializeField] GameObject _hitEffectSpark;
+    [SerializeField] GameObject _muzzleFlash;
+
+    bool canShoot = true;
+    bool isSprinting = false;
+
+    float fPlayerSpeedOrig; // stores the starting player speed
+    int iPlayerHealthOrig; // stores the starting player health
+    int iTimesJumped;
+    
+    Vector3 _playerVelocity;
+    Vector3 _move;
+    Vector3 _playerSpawnPos;
+
+    // Called at Start
     void Start()
     {
-        HPorig = HP;
-        playerSpawnPos = transform.position;
+        // Store starting values for important variables
+        iPlayerHealthOrig = iPlayerHealth;
+        fPlayerSpeedOrig = fPlayerSpeed;
+        _playerSpawnPos = transform.position;
     }
 
-    // Update is called once per frame
+    // Called every frame
     void Update()
     {
-        
+        if (!GameManager._instance.isPaused) // if paused - disable player actions
+        {
+            MovePlayer();
+            Sprint();
+            StartCoroutine(Shoot());
+        }
     }
-    public void takeDamage(int dmg)
+
+    private void MovePlayer()
     {
-        StartCoroutine(DamageFlash());
+        if ((_controller.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            _playerVelocity.y -= 2;
+        }
+
+        // if we land - reset player velocity and reset jump counter
+        if (_controller.isGrounded && _playerVelocity.y < 0)
+        {
+            iTimesJumped = 0;
+            _playerVelocity.y = 0f;
+        }
+
+        // receive input from Unity input manager
+        _move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+
+        // Add our move vector into the character controller move
+        _controller.Move(_move * Time.deltaTime * fPlayerSpeed);
+
+        // Make player jump and increment jump counter
+        if (Input.GetButtonDown("Jump") && iTimesJumped < iJumps)
+        {
+            iTimesJumped++;
+            _playerVelocity.y = fJumpHeight;
+        }
+
+        // add gravity
+        _playerVelocity.y -= fGravityValue * Time.deltaTime;
+
+        // add gravity back into the character controller move
+        _controller.Move(_playerVelocity * Time.deltaTime);
     }
-    IEnumerator DamageFlash()
+
+    void Sprint()
+    {
+        // on down press of 'Sprint' key, increase player speed
+        if (Input.GetButtonDown("Sprint"))
+        {
+            isSprinting = true;
+            fPlayerSpeed = fPlayerSpeed * fSprintMulti;
+        }
+        // on release of 'sprint' key, return player speed to normal
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            isSprinting = false;
+            fPlayerSpeed = fPlayerSpeedOrig;
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        // get input for shooting
+        if (Input.GetButton("Shoot") && canShoot)
+        {
+            // turns shooting off so it cant be immediately executed again
+            canShoot = false;
+
+            RaycastHit hit;
+
+            // Casts a ray from the player camera and performs an action where the ray hits
+            if (Physics.Raycast(UnityEngine.Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)), out hit))
+            {
+                // play spark effect where ray hits
+                Instantiate(_hitEffectSpark, hit.point, _hitEffectSpark.transform.rotation);
+
+                // if the target is damageable, it takes damage
+                if (hit.collider.GetComponent<IDamageable>() != null)
+                {
+                    // get target
+                    IDamageable isDamageable = hit.collider.GetComponent<IDamageable>();
+
+                    // check for body shot or head shot
+                    if (hit.collider is SphereCollider) // apply damage for head shot
+                    {
+                        isDamageable.TakeDamage(10000);
+                    }
+                    else
+                    {
+                        isDamageable.TakeDamage(iWeaponDamage); // apply damage for body shot
+                    }
+                }
+            }
+            // muzzle flash effect
+            _muzzleFlash.transform.localRotation = Quaternion.Euler(0, 0, Random.Range(0, 360)); // rotate effect along Z-axis randomly for every shot
+            _muzzleFlash.SetActive(true);
+            yield return new WaitForSeconds(0.05f);
+            _muzzleFlash.SetActive(false);
+
+            // Timer runs, based off of player shootRate, and then re-enables shooting
+            yield return new WaitForSeconds(fShootRate);
+            canShoot = true;
+        }
+    }
+
+    public void TakeDamage(int iDmg)
+    {
+        iPlayerHealth -= iDmg; 
+        UpdateHealthBar(); 
+        StartCoroutine(DamageFlash()); // flash screen red when damage is taken
+
+        if (iPlayerHealth <= 0) 
+        {
+            GameManager._instance.PlayerDead(); 
+        }
+    }
+    IEnumerator DamageFlash() // Helper function that creates flash upon taking damage
     {
         GameManager._instance._playerDamageFlash.SetActive(true);
         yield return new WaitForSeconds(0.1f);
@@ -34,11 +175,16 @@ public class playerController : MonoBehaviour
     
     public void Respawn()
     {
-        HP = HPorig;
-        //updatePLayerHP();
-        controller.enabled = false;
-        transform.position = playerSpawnPos;
-        controller.enabled = true;
+        iPlayerHealth = iPlayerHealthOrig; // reset player health
+        _controller.enabled = false;
+        transform.position = _playerSpawnPos; // reset player location
+        _controller.enabled = true;
 
+        UpdateHealthBar();
+    }
+
+    public void UpdateHealthBar() // update the healthbar in the UI to reflect player's current health
+    {
+        GameManager._instance._HpBar.fillAmount = (float)iPlayerHealth / (float)iPlayerHealthOrig;
     }
 }
