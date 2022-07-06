26122850144b9b52e3641735eaba17f15d6c79ed
Collin Leavitt <zanthor0128@gmail.com>
2022-07-05 21:12:11 -0400

    Finished the GameManager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
+    public static GameManager _instance;
 
-    [SerializeField] public GameObject _player;
-    [SerializeField] public playerController _playerScript;
+    [Header("Player Referance\n------------------------------")]
+    public GameObject _player;
+    public playerController _playerScript;
 
-    public GameObject pauseMenu;
-    public GameObject playerDeadMenu;
-    public GameObject playerDamageFlash;
+    [Header("UI\n------------------------------")]
     public bool isPaused = false;
-    public Image HPBar;
+    public GameObject _pauseMenu;
+    public GameObject _playerDeadMenu;
+    public Image _HpBar;
+
+    [Header("Effects\n------------------------------")]
+    public GameObject _playerDamageFlash;
 
     void Awake()
     {
-        instance = this;
+        _instance = this;
         _player = GameObject.FindGameObjectWithTag("Player");
-        _playerScript = player.GetComponent<playerController>();
+        _playerScript = _player.GetComponent<playerController>();
     }
 
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && !_playerDeadMenu.activeSelf)
+        {
+            if (!isPaused)
+            {
+                isPaused = true;
+                _pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        _pauseMenu.SetActive(false);
+        _playerDeadMenu.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void PlayerDead()
+    {
+        isPaused = true;
+        _playerDeadMenu.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
 }
