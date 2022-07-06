a98f14644abce7725920d0c1af47866cd0db3268
ktemplar133 <ktemplar@student.fullsail.edu>
2022-07-06 10:22:35 -0700

    adding scripts

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void Resume()
+    {
+        GameManager._instance.Resume();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Respawn()
+    {
+        GameManager._instance._playerScript.Respawn();
+        GameManager._instance.Resume();
+    }
+}
