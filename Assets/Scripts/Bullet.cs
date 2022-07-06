500deed25514d5f1ecc903382141e67d1add7162
JoseDavila0092 <JoseDavila0092@gmail.com>
2022-07-06 18:04:46 -0400

    Added bullet and floor prefabs. Created 3 extra layers to solve the enemy killing itself

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("Values\n------------------------------")]
+    [SerializeField] int iDamage;
+    [SerializeField] int iSpeed;
+    [SerializeField] int iDestroyTime;
+
+    [Header("Body\n------------------------------")]
+    [SerializeField] Rigidbody _rigidBody;
+
+    [Header("Effect\n------------------------------")]
+    [SerializeField] GameObject _hitEffect;
+
+    private void Start()
+    {
+        _rigidBody.velocity = (GameManager._instance._player.transform.position - transform.position).normalized * iSpeed;
+        Destroy(gameObject, iDestroyTime);
+    }
+
+    private void OnTriggerEnter(Collider _collider)
+    {
+        if( _collider.GetComponent<IDamageable>() != null)
+        {
+            IDamageable isDamageable = _collider.GetComponent<IDamageable>();
+            isDamageable.TakeDamage(iDamage);
+        }
+
+        Instantiate(_hitEffect, transform.position, _hitEffect.transform.rotation);
+        Destroy(gameObject);
+    }
+}
