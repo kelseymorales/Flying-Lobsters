971ddc2d744be707c09c12307b457686dc171d46
ktemplar133 <ktemplar@student.fullsail.edu>
2022-07-06 10:19:02 -0700

    all menu and button functions

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+    [SerializeField] int HP;
+    int HPorig;
+    Vector3 playerSpawnPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HPorig = HP;
+        playerSpawnPos = transform.position;
     }
 
     // Update is called once per frame
@@ -24,5 +30,15 @@ public class playerController : MonoBehaviour
         GameManager._instance._playerDamageFlash.SetActive(true);
         yield return new WaitForSeconds(0.1f);
         GameManager._instance._playerDamageFlash.SetActive(false);
+    }
+    
+    public void Respawn()
+    {
+        HP = HPorig;
+        //updatePLayerHP();
+        controller.enabled = false;
+        transform.position = playerSpawnPos;
+        controller.enabled = true;
+
     }
 }
