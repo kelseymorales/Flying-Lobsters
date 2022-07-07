526c8eca4bb805d8c6f00e685ea3b46cddec2d4d
Raymond White <RJWhite4@student.fullsail.edu>
2022-07-06 20:20:57 -0600

    added comments to enemyAI scripts

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -28,6 +28,7 @@ public class EnemyAI : MonoBehaviour, IDamageable
 
     void Update()
     {
+        //agent lets enemies know where player is
         nAgent.SetDestination(GameManager._instance._player.transform.position);
 
         if (nAgent.remainingDistance <= nAgent.stoppingDistance && bCanShoot)
@@ -36,22 +37,27 @@ public class EnemyAI : MonoBehaviour, IDamageable
 
     public void TakeDamage(int iDamage)
     {
+        //when enemy takes damage it flashes a color
         iHP -= iDamage;
         StartCoroutine(FlashColor());
 
+        //if enemy dies then enemy object is destroyed
         if(iHP <= 0)
             Destroy(gameObject);
     }
 
     IEnumerator FlashColor()
     {
+        //flash color when hit
         rRend.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
+        //return back to original color
         rRend.material.color = _enemyColor;
     }
 
     IEnumerator Shoot()
     {
+        //enemy can shoot player
         bCanShoot = false;
         Instantiate(gBullet, transform.position, gBullet.transform.rotation);
         yield return new WaitForSeconds(fShootRate);
