49eedf60e807106231fa0e859a426ec124140391
Collin Leavitt <zanthor0128@gmail.com>
2022-07-20 10:23:24 -0400

    Created spawner logic for when the player enters the trigger area. Added variables to handle how many enemies to spawn what interval to spawn them and how frequently to start the waves

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,15 +1,59 @@
 using UnityEngine;
+using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private int iTriggerSize = 1;
+    [SerializeField] private float fTimeBetweenWaves;
+    [SerializeField] private float fSpawnInterval;
+    [SerializeField] private int iNumOfEnemies;
+    [SerializeField] private GameObject[] _enemies;
 
-    private BoxCollider trigger;
+
+    bool canSpawn;
+
+    private BoxCollider _trigger;
+    private int iEnemiesSpawned;
 
     private void Start() 
     {
-        trigger = GetComponent<BoxCollider>();
-        trigger.size = new Vector3(iTriggerSize, 1, iTriggerSize); 
+        _trigger = GetComponent<BoxCollider>();
+        _trigger.size = new Vector3(iTriggerSize, 1, iTriggerSize); 
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            canSpawn = true;
+            StartCoroutine(StartSpawning());
+        }    
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            canSpawn = false;
+        }
+    }
+
+    private IEnumerator StartSpawning()
+    {
+        while(canSpawn)
+        {
+            yield return new WaitForSeconds(fSpawnInterval);
+
+            Instantiate(_enemies[Random.Range(0, _enemies.Length)]);
+
+            iEnemiesSpawned++;
+
+            if (iEnemiesSpawned == iNumOfEnemies)
+            {
+                yield return new WaitForSeconds(fTimeBetweenWaves);
+                iEnemiesSpawned = 0;
+            }
+        }
     }
 
     public void OnDrawGizmos() 
