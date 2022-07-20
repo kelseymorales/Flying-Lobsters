9e7428474d0fc5c24043695c1416ba14184e3811
Collin Leavitt <zanthor0128@gmail.com>
2022-07-20 10:04:21 -0400

    Added a size variable to the Spawner control the size of the box collider used to start spawning enemies

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    
+    [SerializeField] private int iTriggerSize = 1;
+
+    private BoxCollider trigger;
+
+    private void Start() 
+    {
+        trigger = GetComponent<BoxCollider>();
+        trigger.size = new Vector3(iTriggerSize, 1, iTriggerSize); 
+    }
+
+    public void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, new Vector3(iTriggerSize, 1, iTriggerSize));    
+    }
 }
