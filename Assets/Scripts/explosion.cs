0c1da0cc8779ea5eebdb93caa592ac4983b6240a
Tyler Clardy <tclards@gmail.com>
2022-08-03 12:37:31 -0500

    Commented explosion script

--- Assets/Scripts/explosion.cs
+++ Assets/Scripts/explosion.cs
@@ -2,21 +2,25 @@ using UnityEngine;
 
 public class explosion : MonoBehaviour
 {
-    [SerializeField] int iDamage;
+    [SerializeField] int iDamage; // stores damage dealt by explosion
 
     [Header("Audio")]
     [Header("--------------------------")]
-    public AudioSource aud;
+    public AudioSource aud;     // explosion audio source
+
+    // explosion audio clip and clip volume
     [SerializeField] AudioClip[] aExplosionSound;
     [Range(0.0f, 1.0f)][SerializeField] float aExplosionSoundVol;
 
     void Start()
     {
+        // when explosion is activated, play explosion audio clip
         aud.PlayOneShot(aExplosionSound[Random.Range(0, aExplosionSound.Length)], aExplosionSoundVol);
     }
 
     public void OnTriggerEnter(Collider other)
     {
+        // apply physics pushback to player character, if player character is in range of explosion
         if (other.CompareTag("Player"))
         {
             GameManager._instance._playerScript.vPushBack =
