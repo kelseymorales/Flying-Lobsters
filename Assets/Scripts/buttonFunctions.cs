c4dad3914a4cc3c42d6f9bbc8db83b28f2e6feb8
ktemplar133 <ktemplar@student.fullsail.edu>
2022-08-03 11:28:21 -0700

    comment previous code

--- Assets/Scripts/buttonFunctions.cs
+++ Assets/Scripts/buttonFunctions.cs
@@ -3,23 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
-    public void Resume()
+    public void Resume() //calling resume from game manager for menu UI
     {
         GameManager._instance.Resume();
     }
 
-    public void Quit()
+    public void Quit() //calling quit from game manager for menu UI
     {
         Application.Quit();
     }
 
-    public void Respawn()
+    public void Respawn() //calling respawn from playerscript and resume from game manager for menu UI
     {
         GameManager._instance._playerScript.Respawn();
         GameManager._instance.Resume();
     }
 
-    public void Restart()
+    public void Restart() //calling restart from gamemanger and reloading scene for menu UI
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // reload scene
         GameManager._instance.Restart();
