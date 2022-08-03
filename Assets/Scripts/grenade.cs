80e27e285123ad936bc6fb31db8821d8d08016b2
Tyler Clardy <tclards@gmail.com>
2022-08-03 12:40:09 -0500

    Commented grenade script

--- Assets/Scripts/grenade.cs
+++ Assets/Scripts/grenade.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class grenade : MonoBehaviour
 {
-    [SerializeField] Rigidbody _rigidBody;
-    [SerializeField] int iSpeed;
-    [SerializeField] int iTimer;
+    [SerializeField] Rigidbody _rigidBody;      // stores rigidbody for grenade object
+    [SerializeField] int iSpeed;                // stores the speed at which the grenade moves through worldspace
+    [SerializeField] int iTimer;                // timer value that counts down to grenade exploding
 
-    [SerializeField] GameObject gExplosion;
+    [SerializeField] GameObject gExplosion;     // stores explosion effect for grenade detonation
 
     // Start is called before the first frame update
     void Start()
     {
         // When spawned grenade will move forward and up, arcing towards player
         _rigidBody.velocity = ((GameManager._instance._player.transform.position - transform.position) + new Vector3(0, 0.5f, 0) * iSpeed);
+
+        // start timer countdown for grenade exploding
         StartCoroutine(explosionTime());
     }
 
     IEnumerator explosionTime()
     {
+        // timer countdown
         yield return new WaitForSeconds(iTimer);
+
+        // spawn explosion effect at grenade position
         Instantiate(gExplosion, transform.position, gExplosion.transform.rotation);
 
+        // destroy grenade game object
         Destroy(gameObject);
     }
 }
