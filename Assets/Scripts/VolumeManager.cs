58454e30b3a7193822701442fdc60e3099c1c231
JoseDavila0092 <JoseDavila0092@gmail.com>
2022-08-04 17:53:23 -0400

    Audio mixers set, VolumeManager script was updated to control volume through the mixers. Now the audio divides the main slider into SFX and Music.

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -2,37 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio; 
 
 public class VolumeManager : MonoBehaviour
 {
-    [SerializeField] Slider mainVolumeSlider; //To access the slider
+    [SerializeField] string sVolumeParameter = "MasterVolume"; //String to save the playerPreferences. 
+    [SerializeField] Slider _volumeSlider; //To access the slider
+    [SerializeField] AudioMixer _mixer; // To access the mixer 
+    [SerializeField] private Toggle _toggle; //Toggle used to mute the audio completely
+    private bool isToggleEventDisable; //Boolean to check if the toggle is disabled. 
 
+    private void Awake() 
+    {
+        _volumeSlider.onValueChanged.AddListener(HandleSliderValueChanged);
+        _toggle.onValueChanged.AddListener(HandleToggleValueChanged);
+    }
+
+    private void HandleSliderValueChanged(float value) //Will convert the change in the slider (value 1-0) to decibels
+    {
+        if (value <= 0)
+            _mixer.SetFloat(sVolumeParameter, -75.0f); 
+        else
+            _mixer.SetFloat(sVolumeParameter, Mathf.Log10(value) * 20);
+        isToggleEventDisable = true; 
+        _toggle.isOn = _volumeSlider.value > _volumeSlider.minValue;
+        isToggleEventDisable = false; 
+    }
+
+    private void HandleToggleValueChanged(bool enableSound) //Handles the muting and setting back the sound to a value using the toggle
+    {
+        if (isToggleEventDisable)
+            return; 
+        if (enableSound)
+            _volumeSlider.value = _volumeSlider.maxValue;
+        else
+            _volumeSlider.value = _volumeSlider.minValue; 
+    }
+
+    private void OnDisable() //Sets the preference so the next time the user opens the game the option changes are saved
+    {
+        PlayerPrefs.SetFloat(sVolumeParameter, _volumeSlider.value);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        if (!PlayerPrefs.HasKey("mainVolume"))
-        {
-            PlayerPrefs.SetFloat("mainVolume", 1);
-            Load();
-        }
-        else
-            Load(); 
+        _volumeSlider.value = PlayerPrefs.GetFloat(sVolumeParameter, _volumeSlider.value); 
     }
 
-    public void ChangeMainVolume() //Changes the main volume based on slider
-    {
-        AudioListener.volume = mainVolumeSlider.value;
-        Save();
-
-    }
-
-    private void Save() //Saves user preferences so the user doesn't need to change it over and over again
-    {
-        PlayerPrefs.SetFloat("mainVolume", mainVolumeSlider.value);
-    }
-
-    private void Load() //Loads preferences
-    {
-        mainVolumeSlider.value = PlayerPrefs.GetFloat("mainVolume");
-    }
+    
 }
