8766282dc63de9f2e21f10e8bc58980cb5abf9d2
Tyler Clardy <tclards@gmail.com>
2022-08-07 19:44:55 -0500

    Full functionality for Sniper - updates include: playerController script, and player prefab. gunStats Script gunStat prefabs for shotgun and sniper Tyler Scene gunPickup script UI - added new tag for HUD objects Input Manager - mapped zoom function to "F" - but only when using a sniper

--- Assets/Scripts/gunPickup.cs
+++ Assets/Scripts/gunPickup.cs
@@ -8,9 +8,28 @@ public class gunPickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // check if player collided with pickup
         {
+            // apply gunStats from pickup to player using player's gunPickup function
             GameManager._instance._playerScript.gunPickup(gunStat.fFireRate, gunStat.iDamage, gunStat.gGunModel, gunStat.iClipSize, gunStat.fGunRange, gunStat.aGunShot, gunStat.aGunShotVol, gunStat._anim);
+
+            // update player bools on weapon type
+            if (gunStat.isSniper == true)
+            {
+                GameManager._instance._playerScript.sniperGun = true;
+                GameManager._instance._playerScript.ShotgunGun = false;
+            }
+            else if (gunStat.isShotgun == true)
+            {
+                GameManager._instance._playerScript.ShotgunGun = true;
+                GameManager._instance._playerScript.sniperGun = false;
+            }
+            else
+            {
+                GameManager._instance._playerScript.sniperGun = false;
+                GameManager._instance._playerScript.ShotgunGun = false;
+            }
+
         }
     }
 }
