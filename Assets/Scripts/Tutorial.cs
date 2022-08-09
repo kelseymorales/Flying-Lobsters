8576a7521c26f3e70bafa20b3d8aebb1cedd36f4
Collin Leavitt <zanthor0128@gmail.com>
2022-08-09 17:16:19 -0400

    Created start of the tutorial scene

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializedField] private GameObject moveUi;
+    [SerializedField] private GameObject dogeUi;
+    [SerializedField] private GameObject AttackUi;
+}
