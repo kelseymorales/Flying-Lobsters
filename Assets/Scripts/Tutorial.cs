fbcb1c261599042cc055ffb4c59f3cf3c5263628
Collin Leavitt <zanthor0128@gmail.com>
2022-08-09 17:58:40 -0400

    Created tutorial screens for move and the complete logic for the move and doge function

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -7,4 +7,57 @@ public class Tutorial : MonoBehaviour
     [SerializedField] private GameObject moveUi;
     [SerializedField] private GameObject dogeUi;
     [SerializedField] private GameObject AttackUi;
+    [SerializedField] private GameObject reloadUi;
+
+    private bool[] checks = new bool[4];
+
+
+    private void Update() 
+    {
+        if (moveUi.activeSelf)
+        {
+            if (Input.GetButtonDown("W"))
+            {
+                checks[0] = true;
+            }
+            if (Input.GetButtonDown("S"))
+            {
+                checks[1] = true;
+            }
+
+            if (Input.GetButtonDown("A"))
+            {
+                checks[2] = true;
+            }
+
+            if (Input.GetButtonDown("D"))
+            {
+                checks[3] = true;
+            }
+
+            for (int i = 0; i < checks.Length; i++)
+            {
+                if (checks[i] == false)
+                {
+                    return;
+                }
+            }
+
+            moveUi.SetActive(false);
+            dogeUi.SetActive(true);
+
+            checks = new bool[1];
+        }
+
+        if (dogeUi.activeSelf)
+        {
+            if (Input.GetButtonDown("Doge"))
+            {
+                dogeUi.SetActive(false);
+                AttackUi.SetActive(true);
+
+                checks = new bool[2];
+            }
+        }
+    }
 }
